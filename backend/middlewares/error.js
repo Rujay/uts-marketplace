5d36297077dcc6be3af3b4a4b32da172be2248a5
Rujay <rujayw14@gmail.com>
2021-09-18 14:30:04 +0100

    Separated Production & Development errors

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -4,10 +4,28 @@ const ErrorHandler = require ('../utils/errorHandler');
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-    err.message = err.message || 'Internal server error';
+    
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message, 
+            stack: err.stack
+        })
+    }
 
-    res.status(res.statusCode).json({
-        success: false,
-        error: err.stack
-    })
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+        let error = {...err}
+
+        error.message = err.message
+        
+        res.status(res.statusCode).json({
+            success: false,
+            error: error.message || 'Internal Server Error'
+        })
+        
+    }
+
+
+    
 }
